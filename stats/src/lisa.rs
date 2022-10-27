70f1014a4a38cef0c7302c42ecaef49e6a0b8054
Stuart Lynn <stuart.lynn@gmail.com>
2022-10-27 17:12:31 -0400

    Cleanup and light documentation

--- stats/src/lisa.rs
+++ stats/src/lisa.rs
@@ -1,6 +1,6 @@
 use geo_weights::weights::{TransformType, Weights};
 use nalgebra::DVector;
-use rand::seq::{SliceRandom, IteratorRandom, index::sample};
+use rand::seq::index::sample;
 use rayon::prelude::*;
 use serde::Serialize;
 
@@ -8,16 +8,23 @@ use serde::Serialize;
 pub struct LISAResult {
     pub moran_val: Vec<f64>,
     pub quads: Vec<Quad>,
+    pub lags: Vec<f64>,
     pub p_vals: Vec<f64>,
     pub sims: Vec<Vec<f64>>,
 }
 
+/// Specifies the permutation method to use when performing significance tests 
+/// if it's FULL we calculate a random conditional permutation of the observations for every single
+/// simulation draw
+/// If it's LOOKUP, we generate a set of permutaitons ahead of time corrisoponding to each set of
+/// no of neighbors and then this is used as a look up when performing the simulation.
 #[derive(Debug, Serialize)]
 pub enum PermutationMethod{
     FULL,
     LOOKUP
 }
 
+/// Moran Quad definitions
 #[derive(Debug, Serialize)]
 pub enum Quad {
     HH,
@@ -26,20 +33,26 @@ pub enum Quad {
     LL,
 }
 
+/// Generates a nested set of vectors which contain permuted indices for each of the different
+/// number of neighbors that we have in the set. These are used to speed up selection of neighbors
+/// during the simulation phase of lisa generation
 pub fn generate_perturbation_lookups( max_no_neighbors: usize, permutations:usize, no_observations: usize)-> Vec<Vec<Vec<usize>>>{
-    let lookup: Vec<Vec<Vec<usize>>> = (0..max_no_neighbors+1).into_par_iter().map(|no_neighbors|{
+    (0..max_no_neighbors+1).into_par_iter().map(|no_neighbors|{
         let mut rng = rand::thread_rng();
-        let inner_lookup: Vec<Vec<usize>> = (0..permutations).map(|_|{
-            let sample = sample(&mut rng, no_observations-1, no_neighbors).into_vec();
-            sample
-        }).collect();
-        inner_lookup
-    }).collect();
-    println!("Lookup dim 1: {} 2: {} 3: {} observations {}", lookup.len(), lookup[0].len(), lookup[0][0].len(), no_observations);
-    lookup
+        (0..permutations).map(|_|{
+            sample(&mut rng, no_observations-1, no_neighbors).into_vec()
+        }).collect()
+    }).collect()
 }
 
 
+/// Computes the LISA stats for the given weights and values. Results are a LisaResult object that
+/// contains the 
+/// - moran_values: for each observation
+/// - lags: the lag value for each observation
+/// - quads: the moran quad specification for each observation, 
+/// - p_vals: the estimated p_val of each observation 
+/// - sims: the simulated moran values for each observation if keep_sims is specified
 pub fn lisa(weights: &Weights, values: &[f64], permutations: usize, keep_sims: bool, permutation_method: PermutationMethod) -> LISAResult {
     let x = DVector::from_column_slice(values);
     let no_observations = x.len();
@@ -117,13 +130,19 @@ pub fn lisa(weights: &Weights, values: &[f64], permutations: usize, keep_sims: b
 
             let p_val = (larger as f64 + 1.0) / (permutations as f64 + 1.0);
 
-            (p_val, sim_vals)
+            if keep_sims{
+                (p_val, sim_vals)
+            }
+            else{
+                (p_val,vec![])
+            }
         })
         .collect();
 
     LISAResult {
         moran_val: results.data.into(),
         quads,
+        lags: lags.data.into(),
         sims: sim_results.iter().map(|r| r.1.clone()).collect(),
         p_vals: sim_results.iter().map(|r| r.0).collect(),
     }
